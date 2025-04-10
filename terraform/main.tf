750c4938fe51d30f474e9f695a6d803357e4424a
Randeep Puthan Purakkal <randeepputhan@gmail.com>
2025-04-10 17:35:09 +0530

    adding more environments in workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,21 @@
 resource "aws_ssm_parameter" "name" {
   name     = "test"
   type     = "String"
-  value    = "action test"
+  value    = "action test east"
   provider = aws.east-provider
   tags = {
     Name        = "test"
     Environment = "East"
+  }
+}
+
+resource "aws_ssm_parameter" "name" {
+  name     = "test"
+  type     = "String"
+  value    = "action test west"
+  provider = aws.west-provider
+  tags = {
+    Name        = "test"
+    Environment = "West"
   }
 }
