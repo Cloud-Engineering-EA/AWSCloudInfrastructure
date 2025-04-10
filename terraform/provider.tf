1554d625912faa8863841b173bb17119e8947d08
Randeep Puthan Purakkal <randeepputhan@gmail.com>
2025-04-10 14:36:12 +0530

    Adding terraform code for providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,4 +2,25 @@ terraform {
   backend "s3" {
 
   }
+  required_providers {
+    # AWS provider Version 
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
+# Configure AWS Provider for AWS East 
+provider "aws" {
+  region = "us-east-1"
+  alias  = "east-provider"
+}
+
+# Configure AWS Provider for AWS East 
+provider "aws" {
+  region = "us-west-1"
+  alias  = "west-provider"
+}
+
+
